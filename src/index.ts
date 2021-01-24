dfc683fa55a075edbfa7190a669f8a88dbcb8561
Logan Smithson <logan3713@gmail.com>
2021-01-25 09:35:20 +1100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import TodoList from './todoList';
+import TodoList, { Task } from './todoList';
 
-let todo = new TodoList();
+const todoList = new TodoList();
+
+
+const listElement = document.getElementById("#todoList");
+if (!listElement) {
+  throw new Error('No "#todoList" element');
+}
+
+todoList.tasks.forEach(task => {
+  listElement.innerHTML += taskElement(task);
+});
+
+const taskElement = (task: Task) => {
+  return `<div>
+  <p>${task.taskName}</p><button>${task.isDone}</button>
+</div>`
+}
